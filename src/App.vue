7d48b9fadc946c5cfffcca8bb8ff70101b0dc753
ImssyTnT <562285429@qq.com>
2022-07-02 15:14:14 +0800

    过滤器作业更新

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <h1>使用同一个过滤器处理1876986粉丝数量</h1>
       <span>国内网站:粉丝数量:{{ msg | num(3, '万') }}</span>
       <br />
-      <span>国外网站:粉丝数量:{{ msg | num(4, '千') }}</span>
+      <span>国外网站:粉丝数量:{{ msg | num(2, '千') }}</span>
     </div>
     <div>
       <h1>使用同一个过滤器处理1636450540055时间戳</h1>
@@ -26,16 +26,10 @@ export default {
   },
   filters: {
     num(val, index, str) {
-      let arr = val.split('');
-      const newArr = [];
-      arr.forEach((item, i) => {
-        if (i === index) {
-          newArr.push('.');
-        }
-        newArr.push(item);
-      });
-      newArr.push(str);
-      return newArr.join('');
+      const arr = val.split('');
+      arr.splice(arr.length - 1 - index, 0, '.');
+      arr.push(str);
+      return arr.join('');
     },
     getTime(val, layout) {
       return moment(val).format(layout);
