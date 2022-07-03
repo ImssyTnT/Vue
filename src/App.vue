d4d8bfcca0b7efc42f2f786f9b4fcf2eba2318f4
ImssyTnT <562285429@qq.com>
2022-07-03 21:26:16 +0800

    完成了todo案例

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,81 @@
 <template>
-  <div></div>
+  <div>
+    <TodoHeader @add="addFn" @allTrue="allTrueFn"></TodoHeader>
+    <TodoMain :list="showList" @del="delFn"></TodoMain>
+    <TodoFooter
+      :count="count"
+      @filterData="filterDataFn"
+      @allDel="allDelFn"
+    ></TodoFooter>
+  </div>
 </template>
 
 <script>
-export default {};
+import TodoHeader from './components/TodoHeader.vue';
+import TodoMain from './components/TodoMain.vue';
+import TodoFooter from './components/TodoFooter.vue';
+export default {
+  data() {
+    return {
+      list: JSON.parse(localStorage.getItem('list')),
+      getList: 'all',
+    };
+  },
+  methods: {
+    // 添加功能
+    addFn(val) {
+      const id = this.list[this.list.length - 1]
+        ? this.list[this.list.length - 1].id + 1
+        : 100;
+      this.list.push({ id, name: val, isDone: false });
+    },
+    // 删除功能
+    delFn(id) {
+      this.list.splice(
+        this.list.findIndex((item) => item.id == id),
+        1
+      );
+    },
+    // 点击切换功能
+    filterDataFn(val) {
+      this.getList = val;
+    },
+    // 清空已完成
+    allDelFn() {
+      this.list = this.list.filter((item) => !item.isDone);
+    },
+    allTrueFn(val) {
+      this.list.forEach((item) => (item.isDone = val));
+    },
+  },
+  computed: {
+    count() {
+      return this.list.filter((item) => !item.isDone).length;
+    },
+    showList() {
+      if (this.getList == 'no') {
+        return this.list.filter((item) => !item.isDone);
+      } else if (this.getList == 'yes') {
+        return this.list.filter((item) => item.isDone);
+      } else {
+        return this.list;
+      }
+    },
+  },
+  components: {
+    TodoHeader,
+    TodoMain,
+    TodoFooter,
+  },
+  watch: {
+    list: {
+      handler(val) {
+        localStorage.setItem('list', JSON.stringify(val || []));
+      },
+      deep: true,
+    },
+  },
+};
 </script>
 
 <style></style>
--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,13 +1,49 @@
 <template>
   <header class="header">
     <h1>todos</h1>
-    <input id="toggle-all" class="toggle-all" type="checkbox" />
+    <input
+      id="toggle-all"
+      class="toggle-all"
+      type="checkbox"
+      v-model="allTrue"
+    />
     <label for="toggle-all"></label>
     <!-- label 可以关联一个表单标签 -->
-    <input class="new-todo" placeholder="输入任务名称-回车确认" autofocus />
+    <input
+      class="new-todo"
+      placeholder="输入任务名称-回车确认"
+      autofocus
+      @keyup.enter="enterFn"
+      v-model.trim="val"
+    />
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      val: '',
+    };
+  },
+  methods: {
+    enterFn() {
+      if (this.val.length === 0) {
+        return alert('请输入信息');
+      }
+      this.$emit('add', this.val);
+      this.val = '';
+    },
+  },
+  computed: {
+    allTrue: {
+      get() {
+        return this.$parent.list.every((item) => item.isDone);
+      },
+      set(val) {
+        this.$emit('allTrue', val);
+      },
+    },
+  },
+};
 </script>
