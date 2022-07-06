4823cbaf6678f31d92990ac4fa9f3cbf13a05a54
ImssyTnT <562285429@qq.com>
2022-07-06 23:39:14 +0800

    完成了Day07作业

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <Login></Login>
+    <FormIn></FormIn>
   </div>
 </template>
 
 <script>
 import Login from './views/LoginIn.vue';
+import FormIn from './views/FormIn.vue';
 export default {
   components: {
     Login,
+    FormIn,
   },
 };
 </script>
--- src/views/FormIn.vue
+++ src/views/FormIn.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>sdfsd</div>
+  <div>
+    <FormInfo>
+      <template v-slot:uname="scope">{{ scope.row.name }}</template>
+      <template v-slot:age="scope">{{ scope.row.age }}</template>
+      <template v-slot:img="scope">
+        <img :src="scope.row.headImgUrl" alt="" v-if="scope.row.type == 1" />
+        <a :href="scope.row.headImgUrl" v-else-if="scope.row.type == 0">{{
+          scope.row.headImgUrl
+        }}</a>
+        <span v-else>{{ scope.row.headImgUrl }}</span>
+      </template>
+    </FormInfo>
+  </div>
 </template>
 
 <script>
-import Login01 from '../'
-export default {};
+import FormInfo from '../components/FormInfo.vue';
+export default {
+  components: {
+    FormInfo,
+  },
+};
 </script>
 
 <style></style>
