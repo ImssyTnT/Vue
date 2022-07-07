cc83e16aaed6c662f86adf72a21d016415ab1023
ImssyTnT <562285429@qq.com>
2022-07-07 19:27:06 +0800

    初始化了项目

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <MyHeader></MyHeader>
+    <MyTable></MyTable>
+    <MyTabBer></MyTabBer>
+  </div>
 </template>
 
 <script>
-export default {};
+import MyHeader from './components/MyHeader.vue';
+import MyTable from './components/MyTable.vue';
+import MyTabBer from './components/MyTabBer.vue';
+export default {
+  components: {
+    MyHeader,
+    MyTable,
+    MyTabBer,
+  },
+};
 </script>
 
 <style></style>
